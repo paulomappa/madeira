84f9023cd8a7a685056b10269006f4964819374f
Prof. Paulo Mappa <paulomappa@gmail.com>
2025-05-11 20:52:24 -0300

    inicio

---
+++ conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Estruturas de Madeira'
+copyright = '2025, Prof. Paulo Mappa'
+author = 'Prof. Paulo Mappa'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser",
+                "nbsphinx"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'sphinxdoc'
+#html_theme = 'epub'
+#html_theme = 'scrolls'
+#html_theme = 'classic'
+#html_theme = 'traditional'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,26 @@
+.. image:: madeira.png
+   :align: right
+   :alt: "Logotipo"
+
+Dimensionamento de Estruturas de Madeira
+==========================================
+
+Material de apoio à disciplina **Estruturas de Madeira** do curso de Engenharia
+Civil do campus Varginha do CEFET-MG.
+
+A referência básica é a **ABNT NBR 7190-1 de 2023**.
+
+
+
+.. toctree::
+   :maxdepth: 2
+    
+   classifica 
+   propriedades
+   tracionadas
+   ligacoes
+    
+
+
+   
+
