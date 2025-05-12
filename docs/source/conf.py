57b411208731dc662880365e16336d8ab9a5c936
Prof. Paulo Mappa <paulomappa@gmail.com>
2025-05-11 22:34:34 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,21 +14,25 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser",
-                "nbsphinx"]
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
-language = 'pt_BR'
+# -- Options for HTML output
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-#html_theme = 'sphinxdoc'
-#html_theme = 'epub'
-#html_theme = 'scrolls'
-#html_theme = 'classic'
-#html_theme = 'traditional'
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
