609fc76994432ff02fa625d5bfee45be80eea38f
paulomappa <paulomappa@gmail.com>
2025-05-13 23:25:08 -0300

    incluido nbsphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'nbsphinx',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
